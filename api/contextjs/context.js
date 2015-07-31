d7e433ff51a357f90f5fec344a35887068d9e976
Felipe Bevilacqua <felipe.alphasoft@gmail.com>
2015-07-31 16:59:13 -0300

    colocacao dos icones nos menus de contexto do painel

--- api/contextjs/context.js
+++ api/contextjs/context.js
@@ -108,11 +108,37 @@ var context = context || (function () {
 				if (typeof data[i].target !== 'undefined') {
 					linkTarget = ' target="'+data[i].target+'"';
 				}
-				//TRACE_DEBUG(data[i].className);
+
+				var iconA='<span class="btn-left glyphicon glyphicon-font ui-sortable-handle"></span>';// letra A
+				var iconT='<span class="btn-left glyphicon glyphicon-trash ui-sortable-handle"></span>';// Trash
+				var iconS='<span class="btn-left glyphicon glyphicon-usd ui-sortable-handle"></span>';// $
+				var icon='';
+				
+				if(data[i].className=='REMOVE_LINE_HEADER'){
+					icon=iconT;
+				}else{
+					var jsonM = data[i].json;
+					if(!empty(jsonM)){
+						jsonM	=	$.parseJSON(base64_decode(jsonM));
+						var attr = $(jsonM).attr('MEASURE_SOURCE');
+						if (typeof attr !== typeof undefined && attr !== false) {
+						    icon=iconS;
+						}else{
+							icon=iconA;
+						}
+					}else{
+						if(data[i].className.substr(0,8)=='Measures'){
+							icon=iconS;
+						}else{
+							icon=iconA;
+						}
+					}
+				}
+				
 				if (typeof data[i].subMenu !== 'undefined') {
-					$sub = ('<li class="dropdown-submenu ' + data[i].className + '"><a tabindex="-1" href="' + data[i].href + '">' + data[i].text + '<i class="fa fa-angle-right"></i></a></li>');
+					$sub = ('<li class="dropdown-submenu ' + data[i].className + '"><a tabindex="-1" href="' + data[i].href + '">' + icon + data[i].text + '<i class="fa fa-angle-right"></i></a></li>');
 				} else {
-					$sub = $('<li class="' + data[i].className + '"><a tabindex="-1" json="'+data[i].json+'" href="' + data[i].href + '"'+linkTarget+'>' + data[i].text + '</a></li>');
+					$sub = $('<li class="' + data[i].className + '"><a tabindex="-1" json="'+data[i].json+'" href="' + data[i].href + '"'+linkTarget+'>' + icon + data[i].text + '</a></li>');
 				}
 				if (typeof data[i].action !== 'undefined') {
 					var actiond = new Date(),
