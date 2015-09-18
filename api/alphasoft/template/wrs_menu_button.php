fcb5046b4c6008f854fd83ce22ab871ac56e46da
msdantas <msdantas@ALPHA-01-PC.ALPHASOFT.local>
2015-09-18 15:06:00 -0300

    Correção Bug - MArcelo Correto

--- api/alphasoft/template/wrs_menu_button.php
+++ api/alphasoft/template/wrs_menu_button.php
@@ -1,41 +1,37 @@
 <ul class="nav nav-pills wrs_menu_footer">
-
-<!--  bkp ICONS MENU OLD
+  <li role="presentation"><a href="run.php?file=WRS_MAIN&class=WRS_MAIN&ncon"><i class="fa fa-home"></i></a></li>
+  <li role="presentation"><a href="#" class="link"><i class="fa fa-file-text-o"></i></a>
   
--->
+  
+  <ul class="wrs_menu_footer_sub nav nav-stacked">
+  		
 
-  <li role="presentation"><a href="run.php?file=WRS_MAIN&class=WRS_MAIN&ncon"><i class="fa default_bk_button_menu" style="
-													    background-image: url(imagens/icons/home.png);
-													"></i></a></li>  
-  <li role="presentation"><a href="#" class="wrs_open_modal"><i class="fa default_bk_button_menu" style="
-													    background-image: url(imagens/icons/open.png);
-													"></i></a></li>
-  <li role="presentation"><a href="#" class="wrs_open_save" data-toggle="modal" data-target="#myModal"><i class="fa default_bk_button_menu" style="
-													    background-image: url(imagens/icons/save.png);
-													"></i></a></li>
-  <li role="presentation"><a href="#"  class="link"><i class="fa default_bk_button_menu" style="
-													    background-image: url(imagens/icons/download.png);
-													"></i></a></li>
-  <li role="presentation"><a href="#" class="config"><i class="fa default_bk_button_menu_alternativo" style="
-													    background-image: url(imagens/icons/config.png);
-													"></i></a></li>
-  <li role="presentation"><a href="run.php?file=WRS_MAIN&class=WRS_MAIN&event=logout"><i class="fa default_bk_button_menu" style="
-													    background-image: url(imagens/icons/exit.png);
-													"></i></a></li> 
-  <style>
-  .default_bk_button_menu{
-													    background-size: 35px 25px;
-													    width: 35px;
-													    height: 25px;
-													    margin-top: 3px;
-  }
-  .default_bk_button_menu_alternativo{
-													    background-size: 25px 25px;
-													    width: 25px;
-													    height: 25px;
-													    margin-top: 3px;
-  }
-  </style>
+        <li><i class="glyphicon glyphicon-flash"></i> Alerts</li>
+        <li><i class="glyphicon glyphicon-link"></i> Links</li>
+        <li><i class="glyphicon glyphicon-list-alt"></i> Reports</li>
+        <li><i class="glyphicon glyphicon-book"></i> Books</li>
+        <li><i class="glyphicon glyphicon-briefcase"></i> Tools</li>
+        <li><i class="glyphicon glyphicon-time"></i> Real-time</li>
+        <li class="painel_administrativo_demo"><i class="glyphicon glyphicon-plus"></i> Demo ADM</li>
+  </ul>
+  </li>
+  <li role="presentation"><a href="#" class="wrs_open_modal"><i class="fa fa-folder-open"></i></a></li>
+  <li role="presentation"><a href="#" class="wrs_open_save" data-toggle="modal" data-target="#myModal"> <i class="fa fa-floppy-o" ></i></a></li>
+  <li role="presentation"><a href="#"  class="link"><i class="fa fa-download"></i></a>
+  
+  
+    <ul class="wrs_menu_footer_sub nav nav-stacked">
+  		
+
+      <li><i class="glyphicon glyphicon-briefcase"></i> Tools</li>
+        <li><i class="glyphicon glyphicon-time"></i> Real-time</li>
+        <li><i class="glyphicon glyphicon-plus"></i> Advanced..</li>
+  </ul>
+  
+  </li>
+  <li role="presentation"><a href="run.php?file=WRS_MAIN&class=WRS_MAIN&event=logout"><i class="fa fa-power-off"></i></a></li>
+  
+  <li role="presentation" class="ui-state-active wrs_panel_info_hover "><!-- Detalhes ao passar o mouse  --></li>
 </ul>
 <div class="rodape_name_right"><span class="glyphicon glyphicon-user"></span> <span class="ui-state-active"><?php echo $HTML_USER_NAME;?></span> | <?php echo $HTML_CUSTOMER_NAME;?>
 
