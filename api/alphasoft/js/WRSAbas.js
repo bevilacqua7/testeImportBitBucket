a0455b9c065de3498b5e2a6617c0cbd87fe52d86
felipe_alphasoft <felipe.alphasoft@gmail.com>
2015-10-02 15:54:50 -0300

    ajuste para detectar melhor aba de reativacao quando apagar uma aba já ativa, utilizando o evento de click para nao ocasionar quebra de processamento do atual fluxo de funcionamento

--- api/alphasoft/js/WRSAbas.js
+++ api/alphasoft/js/WRSAbas.js
@@ -684,13 +684,13 @@ function trace_change_kendo(kendoUi)
 							return true;
 						}
 				
-					var prox_aba = $('li.'+_report_id).next('li');
+					var outra_aba = $('li.'+_report_id).parent().children('li:not(.new_file)').first();
 					var _aba_e_ativa = $('li.'+_report_id).hasClass('active');
 					
 					__remove({'report_id':_report_id});
 					
 					if(_aba_e_ativa){
-						prox_aba.trigger('click');
+						outra_aba.trigger('click');
 					}
 
 			}
